27e753a7c565d73a7f06ae6c0de32877f8f7bb97
gaurav dhanfole <gsdhanfole@gmail.com>
2024-11-22 16:54:57 +0530

    models

--- src/blogapp/models.py
+++ src/blogapp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Blog(models.Model):
     title=models.TextField(max_length=100)
     description=models.TextField(max_length=300)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    image=models.ImageField(upload_to='blog_images/')
 
     class Meta:
         db_table='blogs'
--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('/<int:id>',GetUpdateDeleteBlog.as_view()),
+    path('/<int:pk>',GetUpdateDeleteBlog.as_view()),
     path('',CreateAndGetAllBlogs.as_view())
 ]
