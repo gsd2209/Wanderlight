6472ae2acf6526a1fb1173eadcbb7c15ed4b071a
gaurav dhanfole <gsdhanfole@gmail.com>
2024-12-06 17:30:24 +0530

    otp

---
+++ src/emailapp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .services import send_email_task
+
+class EmailSerializer(serializers.Serializer):
+    recipient_email = serializers.EmailField()
+    subject = serializers.CharField(max_length=255)
+    message = serializers.CharField()
+
+    def send_email(self):
+        from django.core.mail import send_mail
+
+        validated_data = self.validated_data
+        send_email_task.delay(
+            subject=validated_data['subject'],
+            message=validated_data['message'],
+            recipient_email=validated_data['recipient_email'],
+        )
--- src/emailapp/views.py
+++ src/emailapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import EmailSerializer
+
 
 # Create your views here.
+class SendEmailView(APIView):
+    def post(self,request):
+        serializer = EmailSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.send_email()
+            return Response({'message':'Email sent succesfully'},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
